4ac83934368404d15a44378265a9caf4b49898dc
VISHAL DUBEY <77582656+vishaldubey40@users.noreply.github.com>
2021-09-10 12:56:42 +0530

    Add files via upload

---
+++ jdoodle - 2021-09-10T122626.234.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define pi (3.141592653589)
+#define mod 1000000007
+ 
+#define float double
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(c) c.begin(), c.end()
+#define min3(a, b, c) min(c, min(a, b))
+#define min4(a, b, c, d) min(d, min(c, min(a, b)))
+#define rrep(i, n) for(int i=n-1;i>=0;i--)
+#define rep(i,n) for(int i=0;i<n;i++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+ 
+ 
+ 
+ 
+int main()
+{  int t;
+cin>>t;
+while(t--)
+{
+ 
+    // Fuck Ratings, I'm in Love with Experience.
+// Once a Charas, Always a CHARAS.
+ 
+    int a,b,c,d;
+    cin>>a>>b;
+c=(b/2)+1;
+if(c<a){
+    c=a;
+}
+cout<<b%c<<endl;
+}
+return 0;
+}
+ 
+ 
